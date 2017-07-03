c6c959147415411e7669fcca89303a6203e19925
Rodrigo Ovalle <r.ovalle@outlook.com>
2017-07-03 10:51:09 -0300

    Delete obsolete TetriminoDrawer class

--- bootcamp-cpp-tetris-game/main.cpp
+++ bootcamp-cpp-tetris-game/main.cpp
@@ -1,40 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include "TetriminoDrawer.h"
+
 #include "TetrisDrawer.h"
 #include "TetriminoManager.h"
 #include "TetrisBoard.h"
-
-void draw() {
-
-  TetriminoManager mngr{};
-  Tetrimino * t;
-  t = mngr.getRandom();
-
-  sf::RenderWindow window(sf::VideoMode(700, 700), "Ventana", sf::Style::Titlebar | sf::Style::Close);
-  TetriminoDrawer drawer(window);
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        t->rotate();
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
-        t = mngr.getRandom();
-      }
-
-    }
-    window.clear();
-    drawer.draw(*t);
-    window.display();
-
-
-  }
-}
 
 int main()
 {
